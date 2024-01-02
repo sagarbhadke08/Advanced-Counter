53262a7c6a0ef16ee8f7fb397a46b8bcd845d8a8
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-01-02 23:53:04 +0530

    used memo

--- src/components/UI/IconButton.jsx
+++ src/components/UI/IconButton.jsx
@@ -1,6 +1,6 @@
 import { log } from '../../log.js';
 
-export default function IconButton({ children, icon, ...props }) {
+const IconButton = function IconButton({ children, icon, ...props }) {
   log('<IconButton /> rendered', 2);
 
   const Icon = icon;
@@ -11,3 +11,5 @@ export default function IconButton({ children, icon, ...props }) {
     </button>
   );
 }
+
+export default IconButton;
